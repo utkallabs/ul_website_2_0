53806a9b1e9e17faeae4516b304419296fdc1b36
pragatipattanayak <110605178+pragatipattanayak@users.noreply.github.com>
2023-04-26 17:10:37 +0530

    fixissue and add

--- assets/js/main.js
+++ assets/js/main.js
@@ -364,7 +364,8 @@ for (i = 0; i < acc.length; i++) {
 
 
 //// sliderhome
-let slideIndex = 1;
+//// sliderhome
+let slideIndex = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -375,21 +376,24 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
 //// sliderhome////
 
 // function myFunction() {
