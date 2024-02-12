6a53258952094261b7b04abc687bb9ef00a2442a
Suvendu Shekhar Giri <suvendu.giri@live.com>
2024-02-12 10:49:37 +0530

    wrong serverhost rectified

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
 //----------- smtp data start----------
-$mailHost='smtp.googlemail.com';
-$mailPort=465;
-$mailUsername='utkallabs2020@gmail.com';
-$mailPassword='UtkalLabs@2020';
-$mailEncryption='ssl';
+$mailHost = 'smtp.googlemail.com';
+$mailPort = 465;
+$mailUsername = 'utkallabs2020@gmail.com';
+$mailPassword = 'UtkalLabs@2020';
+$mailEncryption = 'ssl';
 //---------- smtp data end-------
 
-$serverHost = 'https://v2.utkallabs.com';
-
-?>
+$serverHost = 'https://utkallabs.com';
