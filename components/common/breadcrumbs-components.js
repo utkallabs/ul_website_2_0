7cc1db0d7b08046cdc479ae002b90eb13aa2ffb3
anpUtkallabs <pandachyuta@gmail.com>
2024-03-29 01:09:17 +0530

    add breadCrumbs components

---
+++ components/common/breadcrumbs-components.js
@@ -0,0 +1,40 @@
+// breadcrumbs-components.js
+class Breadcrumbs extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const title = this.getAttribute("data-title");
+    const address = this.getAttribute("data-address");
+    const file = this.getAttribute("data-file");
+    this.innerHTML = `
+        <div>
+            <h3></h3>
+            <p></p>
+        </div>
+        <<section id="breadcrumbs" class="breadcrumbs">
+        <div class="breadcrumb-hero">
+            <div class="container">
+
+                <h2>About Us</h2>
+                <div class="breadcrumb-area">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="${file}">${title}</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">${address}</li>
+
+                        </ol>
+                    </nav>
+                </div>
+
+            </div>
+    </div>
+
+
+    </section> 
+      `;
+  }
+}
+
+customElements.define("breadcrumbs-component", Breadcrumbs);
