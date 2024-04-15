26cb9111c0778250c2fda70c9fcd05ccc22bc5fa
Susil Kumar Behera <susil.behera@utkallabs.com>
2024-04-15 19:01:21 +0530

    filter , career & service form

--- config.php
+++ config.php
@@ -1,15 +1,22 @@
 <?php
 //----------- smtp data start----------
-$mailHost='smtp.googlemail.com';
+// $mailHost='smtp.googlemail.com';
+// $mailPort=465;
+// $mailUsername='utkallabs2020@gmail.com';
+// //$mailPassword='UtkalLabs@2020';
+// $mailEncryption='ssl';
+// $mailPassword='okfg qlpu gkjm pvkm';
+// $hrMailAddress = 'hr@utkallabs.com';
+// // //---------- smtp data end-------
+
+// $serverHost = 'https://utkallabs.com';
+// $reCaptchaSecretKey = '6LekdHEpAAAAAHM6JUvT3zS2PNI7B6Mcjxu-JokG';
+ $hrMailAddress = 'susilbehera06@gmail.com';
+$reCaptchaSecretKey = '6Lc7SHEpAAAAAO19N2kLeevXn848yTJs9U5SFm_D';
+$mailHost='smtpout.secureserver.net';
 $mailPort=465;
-$mailUsername='utkallabs2020@gmail.com';
-//$mailPassword='UtkalLabs@2020';
+$mailUsername='susil.behera@utkallabs.com';
 $mailEncryption='ssl';
-$mailPassword='okfg qlpu gkjm pvkm';
-
-//---------- smtp data end-------
-
-$serverHost = 'https://utkallabs.com';
-$reCaptchaSecretKey = '6LekdHEpAAAAAHM6JUvT3zS2PNI7B6Mcjxu-JokG';
+$mailPassword='Susil@9146';
 
 ?>
--- sendmail.php
+++ sendmail.php
@@ -21,7 +21,37 @@ if(isset($_POST['g-recaptcha-response'])){
         $name = $_POST['name'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
-        $message = $_POST['message'];
+        if(isset($_POST['message'])){
+            $message = $_POST['message'];
+        }else{
+            $message = "";
+        }
+        if(isset($_POST['position'])){
+            $position = $_POST['position'];
+        }
+        if(isset($_POST['pagename'])){
+            $pagename = $_POST['pagename'];
+        }
+        if(isset($_FILES['fileInput'])){
+            $file = $_FILES['fileInput'];
+            $allowedExtensions = ['pdf', 'doc', 'docx'];
+            $fileExtension = pathinfo($file['name'], PATHINFO_EXTENSION);
+            if (!in_array(strtolower($fileExtension), $allowedExtensions)) {
+                echo json_encode(array(
+                    'responseCode' => 401,
+                    'message' => "Invalid file extension. Allowed extensions: " . implode(", ", $allowedExtensions)
+                ));
+                exit; 
+            }
+            $maxFileSize = 2 * 1024 * 1024; // 2MB
+            if ($file['size'] > $maxFileSize) {
+                echo json_encode(array(
+                    'responseCode' => 401,
+                    'message' => "File size exceeds limit. Max allowed size: 2MB"
+                ));
+                exit; 
+            }
+        }
         
         $mail->isSMTP();
         $mail->Host = $mailHost;
@@ -30,18 +60,30 @@ if(isset($_POST['g-recaptcha-response'])){
         $mail->Password = $mailPassword;
         $mail->SMTPSecure = $mailEncryption;
         $mail->Port = $mailPort;
+
+        if(isset($_FILES['fileInput'])){
+            $mail->addAttachment($file['tmp_name'], $file['name']);
+        }
         
         $mail->setFrom($mailUsername);
         $mail->addAddress($mailUsername);
         
         $mail->isHTML(true);
-        $mail->Subject = 'Message Received (Contact Page)';
-        $mail->Body = "<h3>Name : $name <br> Email : $email <br> Phone : $phone <br> Message : $message </h3>";
+        if(isset($_POST['pagename'])){
+            $mail->addAddress($hrMailAddress);
+            $mail->Subject = 'Message Received (Career Page)';
+            $mail->Body = "<h3>Position : $position <br> Name : $name <br> Email : $email <br> Phone : $phone <br> Description : $message <br> </h3>";
+            $successMsg = 'Thank you! your application successfully submitted.';
+        }else{
+            $mail->Subject = 'Message Received (Contact Page)';
+            $mail->Body = "<h3>Name : $name <br> Email : $email <br> Phone : $phone <br> Message : $message </h3>";
+            $successMsg = 'Message Sent! Thank you for contacting us.';
+        }
         
         if($mail->send()){
             echo json_encode(array(
                 'responseCode' => 200,
-                'message' => 'Message Sent! Thank you for contacting us.'
+                'message' => $successMsg
             ));
         }else{
             echo json_encode(array(
