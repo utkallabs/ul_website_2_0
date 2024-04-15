918870ed9608de12cfb556eb917c963a793c2a7c
Susil Kumar Behera <susil.behera@utkallabs.com>
2024-04-15 19:03:40 +0530

    filter , career & service form -config changes

--- config.php
+++ config.php
@@ -1,22 +1,15 @@
 <?php
 //----------- smtp data start----------
-// $mailHost='smtp.googlemail.com';
-// $mailPort=465;
-// $mailUsername='utkallabs2020@gmail.com';
-// //$mailPassword='UtkalLabs@2020';
-// $mailEncryption='ssl';
-// $mailPassword='okfg qlpu gkjm pvkm';
-// $hrMailAddress = 'hr@utkallabs.com';
-// // //---------- smtp data end-------
-
-// $serverHost = 'https://utkallabs.com';
-// $reCaptchaSecretKey = '6LekdHEpAAAAAHM6JUvT3zS2PNI7B6Mcjxu-JokG';
- $hrMailAddress = 'susilbehera06@gmail.com';
-$reCaptchaSecretKey = '6Lc7SHEpAAAAAO19N2kLeevXn848yTJs9U5SFm_D';
-$mailHost='smtpout.secureserver.net';
+$mailHost='smtp.googlemail.com';
 $mailPort=465;
-$mailUsername='susil.behera@utkallabs.com';
+$mailUsername='utkallabs2020@gmail.com';
+//$mailPassword='UtkalLabs@2020';
 $mailEncryption='ssl';
-$mailPassword='Susil@9146';
+$mailPassword='okfg qlpu gkjm pvkm';
+$hrMailAddress = 'hr@utkallabs.com';
+// //---------- smtp data end-------
+
+$serverHost = 'https://utkallabs.com';
+$reCaptchaSecretKey = '6LekdHEpAAAAAHM6JUvT3zS2PNI7B6Mcjxu-JokG';
 
 ?>
