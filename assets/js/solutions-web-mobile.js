a3d107d021cd4151c801ae79f067b9fca4556bcb
unknown <sradhanjali.kabi@utkallabs.com>
2024-07-30 17:50:52 +0530

    no-result section modified

--- assets/js/solutions-web-mobile.js
+++ assets/js/solutions-web-mobile.js
@@ -21,11 +21,16 @@ if (data.filter(d => ((d.techStack.indexOf("MySQL") != -1) || (d.techStack.index
         ).join("");
 }
 else {
-    document.getElementById("solutions-lists").innerHTML = `<div class="card-wrapper col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-30" data-technology="None">
-                    <div class="card h-100 py-3 shadow border rounded  d-flex">
-                        <img src="https://img.freepik.com/free-vector/hand-drawn-no-data-concept_52683-127823.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1720310400&semt=ais_user" class="card-img-top px-3" alt="...">
-                        <div class="card-body d-flex flex-column bd-highlight">
-                        </div>
-                    </div>
-                </div>`;
+    document.getElementById("solutions-lists").innerHTML = `<div class="shadow py-5">
+        <div class=" justify-content-center d-flex"><img src="assets/images/service/no-result.gif"></div>
+        <div class=" justify-content-center d-flex">
+            <h3>We’re updating!</h3>
+        </div>
+        <div class=" justify-content-center d-flex">
+        <svg height="10" width="200">
+            <line x1="0" y1="10" x2="250" y2="10" style="stroke:#e0e7f9;stroke-width:12" />
+            <line x1="0" y1="10" x2="130" y2="10" style="stroke:#008283;stroke-width:12" />
+            Sorry, your browser does not support inline SVG.
+          </svg></div>
+    </div>`;
 }
